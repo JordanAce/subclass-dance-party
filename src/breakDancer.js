ffbe4a740f846e8f06b6a47aec34314e3c970a15
Jordan <jordanace83@gmail.com>
2021-01-15 14:25:55 -0600

    Messed with CSS and HTML

--- src/breakDancer.js
+++ src/breakDancer.js
@@ -10,6 +10,7 @@ makeBreakDancer.prototype.constructor = makeBreakDancer;
 makeBreakDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   makeDancer.prototype.step.call(this);
+
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
